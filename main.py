baba9a31ee2b741c5f53d747e9ce042a7c7f6e11
Meynrun <ahakahacker@mail.ru>
2023-11-17 15:26:26 +0300

    moved execute to function

--- main.py
+++ main.py
@@ -1,13 +1,24 @@
 import yaml
 import os
 import sys
+import locale
+import webbrowser
 
 sys.stdout.reconfigure(encoding='utf-8')
 
-with open('commands.yaml') as config_file:
+with open('config/commands.yaml', 'r', encoding='utf-8') as config_file:
     commands = yaml.safe_load(config_file)
 
-def execute(command):
+# Set console lang
+if locale.getlocale()[0] == "Russian_Russia":
+    with open('config/lang/ru_RU.yaml', 'r', encoding='utf-8') as lang_file:
+        lang = yaml.safe_load(lang_file)
+else:
+    with open('config/lang/en_US.yaml', 'r', encoding='utf-8') as lang_file:
+        lang = yaml.safe_load(lang_file)
+
+
+def execute(command: str):
     # Checking the existence of the command
     if command in commands:
         # Checking the type of command
@@ -16,10 +27,28 @@ def execute(command):
 
             for app in apps:
                 os.startfile(app)
-            print("Successfully")
-    else:
-        print("This command doesn't exist")
+            print(lang['command.successfully'])
 
+        elif commands[command]['type'] == "steam":
+            id = commands[command]['id']
+            webbrowser.open(f'steam://rungameid/{id}')
+            print(lang['command.successfully'])
+
+    elif command == 'help':
+        for command in commands.keys():
+            print(command)
+        print('help')
+        print('support')
+
+    elif command == 'support':
+        for line in lang['info.support']:
+            print(line)
+
+    else:
+        print(lang['command.notfound'])
+
+
+print(lang['info.welcome'])
 while True:
     # Getting user input
     command = input('> ')

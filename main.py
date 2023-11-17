ceeac92fad0dac0fa3d187950c6a1772d421fe4c
Meynrun <ahakahacker@mail.ru>
2023-11-17 14:14:46 +0300

    moved execute to function

--- main.py
+++ main.py
@@ -7,5 +7,20 @@ sys.stdout.reconfigure(encoding='utf-8')
 with open('commands.yaml') as config_file:
     commands = yaml.safe_load(config_file)
 
-# Getting user input
-command = input('> ')
+def execute(command):
+    # Checking the existence of the command
+    if command in commands:
+        # Checking the type of command
+        if commands[command]['type'] == "app":
+            apps = commands[command]['path']
+
+            for app in apps:
+                os.startfile(app)
+            print("Successfully")
+    else:
+        print("This command doesn't exist")
+
+while True:
+    # Getting user input
+    command = input('> ')
+    execute(command)
